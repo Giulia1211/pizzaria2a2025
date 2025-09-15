edb8618d429ec73b6a3dfc7570090b87212aaf10
Giulia1211 <rm93964@estudante.fieb.edu.br>
2025-09-15 12:28:38 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const Produtos = () => {
+
+    const[dados, setDados] = useState([]);
+
+    const pegarPizzas = () => {
+
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto",
+              {withCredentials: true}
+    )
+
+        .then((response) => setDados(responde.data))
+        .catch((error) => console.log(error))
+    }
+
+    useEffect(() => {
+
+        pegarPizzas ();
+    }, [])
+
 
     //Objeto de produtos na lista 
     const pizzas = [
